236c2e537d6998eecb8c5b40de114155f6bdeb78
Marlon Conejo <mconejoquilindo@gmail.com>
2025-03-29 10:59:13 -0500

    first commit

---
+++ app3.py
@@ -0,0 +1,12 @@
+
+from fastapi import FastAPI
+
+from routers import routerPredictions
+
+
+app = FastAPI()
+app.include_router(routerPredictions.router)
+
+
+if __name__=="__main__":
+    app.run()
